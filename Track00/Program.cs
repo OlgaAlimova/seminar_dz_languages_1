4a856142a2a9b98d7d384cc03d01a5b0edd9e3a5
olga alimova <alim-olgaru@yamdex.ru>
2022-09-16 14:07:52 +0300

    сделаны задачи первого занятия

---
+++ Track00/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Введите целове число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// 2 147 483 648 до 2 147 483 647 - диапазон int
+int square = number * number;
+Console.Write($"квадрат числа {number} = {square}");
+
+//if (number == square) Console.WriteLine(number);
+
+
