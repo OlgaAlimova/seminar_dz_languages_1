6ab63e31d4d91d9074bdbd65e49fca6a221e7810
olga alimova <alim-olgaru@yamdex.ru>
2022-09-21 13:31:21 +0300

    задачи 2го семинара

--- Track03/Program.cs
+++ Track03/Program.cs
@@ -1,3 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N)
 // а на выходе показывает все целые числа в промежутке от -N до N
 
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = -num;
+while (counter <= num)
+{
+    Console.Write($"{counter} ");
+    counter++;
+}
